af4d20b360ae4f20257ed3d9bff9af1197ee04a8
Mahak Mujawadiya <mahak.mujawadiya@lucentinnovation.com>
2023-03-28 10:58:57 +0530

    crud view added

--- librarymanagement/libraryapp/views.py
+++ librarymanagement/libraryapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Book
+from .Bookserializer import BookSerializer
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
 # Create your views here.
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
+    authentication_classes=[]
+    permission_classes=[IsAdminUser]
