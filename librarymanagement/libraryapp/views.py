c2a808b9d9574faaa918f615f2f9f82727837427
Mahak Mujawadiya <mahak.mujawadiya@lucentinnovation.com>
2023-03-28 11:18:12 +0530

    views chsnges

--- librarymanagement/libraryapp/views.py
+++ librarymanagement/libraryapp/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from  .models import Book
-from .Bookserializer import BookSerializer
-from  rest_framework_simplejwt.authentication import JWTAuthentication 
-from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .Bookserializer import BookSerializer
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
+# Create your views here.
 
-class Books(viewsets.ReadOnlyModelViewSet):
+class StudentViewSet(viewsets.ModelViewSet):
     queryset=Book.objects.all()
-    serializer_class= BookSerializer
-    authentication_classes=[JWTAuthentication]
-    permission_classes=[IsAuthenticated]
+    serializer_class=BookSerializer
+    authentication_classes=[]
+    permission_classes=[IsAdminUser]
